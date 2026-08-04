cb45764e945519c63b0a831565e71785ce9509a2
Aditya gupta <adityagupta@Adityas-MacBook-Air.local>
2026-08-05 00:21:25 +0530

    Initial Java Student Registration System

---
+++ Ccontroller.java
@@ -0,0 +1,46 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ccontroller implements ActionListener {
+
+    private final Cmodels cm;
+    private final Cviews cv;
+
+    public Ccontroller(Cmodels cm, Cviews cv) {
+        this.cm = cm;
+        this.cv = cv;
+
+        cv.add.addActionListener(this);
+        cv.sub.addActionListener(this);
+        cv.mul.addActionListener(this);
+        cv.div.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            double a = Double.parseDouble(cv.txta.getText().trim());
+            double b = Double.parseDouble(cv.txtb.getText().trim());
+            double result;
+
+            if (e.getSource() == cv.add) {
+                result = cm.add(a, b);
+            } else if (e.getSource() == cv.sub) {
+                result = cm.subtract(a, b);
+            } else if (e.getSource() == cv.mul) {
+                result = cm.multiply(a, b);
+            } else if (e.getSource() == cv.div) {
+                result = cm.div(a, b);
+            } else {
+                return;
+            }
+
+            cv.txtresult.setText(String.valueOf(result));
+
+        } catch (NumberFormatException ex) {
+            cv.txtresult.setText("Valid numbers enter karo");
+        } catch (ArithmeticException ex) {
+            cv.txtresult.setText(ex.getMessage());
+        }
+    }
+}
---
+++ Cmodels.java
@@ -0,0 +1,22 @@
+public class Cmodels {
+    public double add(double a, double b){
+        return a + b;
+    }
+    
+    public double subtract(double a, double b){
+        if(b > a){
+            return 0;
+        }
+        return a - b;
+    }
+    public double multiply(double a, double b){
+        return a * b;
+    }
+    public double div(double a, double b){
+        if(b == 0){
+            return 0 ;
+        }
+        return a / b;
+    }
+
+}
